4c37bb5006b7a8a71f08ee07e9f9f611fa48dd6f
Kai Strempel <kstrempel@gmail.com>
2018-09-19 20:11:12 +0200

    adds tokenizer and parser

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 pub mod parser;
 
-use std::string::String;
-
 fn main() {
     println!("Hello, rispy!");
-    let mut parser = parser::Parser::new();
-    parser.parse(String::from("hello du da")).is_ok();
+    let _parser = parser::Parser::new();
 }
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -4,22 +4,48 @@ use std::vec::Vec;
 use std::num::ParseIntError;
 
 struct Token {
-    value: String
+    value: String,
+    token: Vec<Vec<Token>>
 }
 
 pub struct Parser {
-    tokens : Vec<Token>
+    tokens : Vec<Token>,
+    tree : Token
 }
 
 impl Parser {
 
     pub fn new() -> Parser {
-        Parser{tokens: Vec::new()}
+        Parser{tokens: Vec::new(), tree: Token{value: String::new(), token: Vec::new()}}
     }
 
-    pub fn parse<'a>(&mut self, code: String) -> Result<(), ParseIntError> {
-        self.tokens.push(Token{value:code});
+    pub fn tokenize(&mut self, code: String) {
+        let code_with_spaces = code.replace("(", "( ").replace(")", " )");
+        for token in code_with_spaces.split(" "){
+            self.tokens.push(Token{value: String::from(token), token: Vec::new()});
+        }
+    }
+
+    pub fn parse(&mut self) -> Result<(), ParseIntError> {
+        self.tree = self.read_from_token();
         Ok(())
+    }
+
+    fn read_from_token(&mut self) -> Token {
+        let mut result = Vec::new();
+        while !self.tokens.is_empty() {
+            let token = self.tokens.remove(0).value;
+            match token.as_str() {
+                "(" => {
+                    let recur = self.read_from_token();
+                    return Token{value: String::new(), token: result.push(recur)}
+                },
+                ")" => return Token{value: String::new(), token: result},
+                _ => result.push(Token{value: token, token: Vec::new()})
+            }
+        };
+
+        Token{value: String::new(), token: result}
     }
 }
 
@@ -28,10 +54,21 @@ mod tests {
     use parser::Parser;
 
     #[test]
+    fn test_tokenizer(){
+        let mut parser = Parser::new();
+        parser.tokenize(String::from("(begin (define r 10) (* pi (* r r)))"));
+        assert_eq!(parser.tokens[0].value, "(");
+        assert_eq!(parser.tokens[1].value, "begin");
+        assert_eq!(parser.tokens[2].value, "(");
+        assert_eq!(parser.tokens[3].value, "define");
+        assert_eq!(parser.tokens[4].value, "r");
+    }
+
+    #[test]
     fn test_parser(){
         let mut parser = Parser::new();
-        let result = parser.parse(String::from("(begin (define r 10) (* pi (* r r)))"));
-        assert!(result.is_ok());
-        assert_eq!(parser.tokens[0].value, "(");
+        parser.tokenize(String::from("(begin (define r 10) (* pi (* r r)))"));
+        assert!(parser.parse().is_ok());
+        assert_eq!(parser.tree.token.unwrap()[0].value, "begin")
     }
 }
