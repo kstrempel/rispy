b98b7ff1a5716e4944cdd47b259e3fb1b1ad6449
Kai Strempel <kstrempel@gmail.com>
2018-09-30 14:23:15 +0200

    adds environment linked list

--- src/runtime/environment.rs
+++ src/runtime/environment.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
-use std::boxed::Box;
-use std::rc::Rc;
+use std::slice::Iter;
+
+use parser::Token;
 
 
 #[derive(Debug)]
@@ -10,7 +11,7 @@ pub enum ResultValue {
     Float(f64),
     Str(String),
     Func(),
-    Error()
+    Error(String)
 }
 
 #[derive(Debug)]
@@ -21,7 +22,7 @@ pub struct Environment<'a> {
 
 impl<'a> Environment<'a> {
 
-    pub fn child(&'a self) -> Self {
+    pub fn child(&'a self) -> Environment {
         Environment{parent: Some(self),
                     values: HashMap::new()}
     }
@@ -31,4 +32,104 @@ impl<'a> Environment<'a> {
                     values: HashMap::new()}
     }
 
+    pub fn get_variable(&'a self, name: &String) -> Result<&ResultValue, String> {
+        if let Some(result_value) = self.values.get(name) {
+            return Ok(result_value);
+        }
+
+        Err(String::from("unknown variable name"))
+    }
+
+    pub fn eval_parser(&mut self, tree: &'a Vec<Token>) -> ResultValue {
+        let mut iter = tree.into_iter();
+        let mut result = ResultValue::None;
+        while let Some(token) = iter.next() {
+            match token {
+                Token::Atom(symbol) => {
+                    result = self.match_atom(symbol, &mut iter);
+                },
+                Token::Subs(subs) => {
+                    let mut child = self.child();
+                    result = child.eval_parser(subs);
+                }
+                _ => {
+                    println!("Panic at {:?}", tree);
+                    result = ResultValue::None;
+                }
+            }
+        }
+
+        result
+    }
+
+    fn match_atom(&mut self, symbol: &String, iter: &mut Iter<Token>) -> ResultValue {
+        let str_symbol = symbol.as_str();
+        match str_symbol {
+            "cons" => {
+                let mut result = String::new();
+                while let Some(token) = iter.next() {
+                    match token {
+                        Token::AtomString(atom) => result.push_str(atom.as_str()),
+                        _ => result.push_str("Wrong parameters for 'cons'")
+                    };
+                };
+                ResultValue::Str(result)
+            },
+            "+" => {
+                let mut result = 0.0;
+                let mut int_only = true;
+                while let Some(token) = iter.next() {
+                    match token {
+                        Token::AtomInt(atom) => result += *atom as f64,
+                        Token::AtomFloat(atom) => {
+                                    int_only = false;
+                                    result += atom
+                        },
+                        Token::Atom(var_name) => {
+                            let value = self.get_variable(var_name).unwrap();
+                            match value {
+                                ResultValue::Int(atom) => result += *atom as f64,
+                                ResultValue::Float(atom) => {
+                                    int_only = false;
+                                    result += atom
+                                },
+                                ResultValue::Error(error) => println!("{}",error),
+                                _ => println!("Panic")
+                            }
+                        },
+                        Token::Subs(tokens) => {
+                            let mut child_environment = self.child();
+                            let result_value = child_environment.eval_parser(tokens);
+                            match result_value {
+                                ResultValue::Int(atom) => result += atom as f64,
+                                ResultValue::Float(atom) => {
+                                    int_only = false;
+                                    result += atom
+                                },
+                                _ => println!("Panic")
+                            }
+                        },
+                        _ => println!("Panic")
+                    };
+                };
+                if int_only {
+                    ResultValue::Int(result as i64)
+                } else {
+                    ResultValue::Float(result)
+                }
+            },
+            "define" => {
+                let _name = iter.next();
+                let _value = iter.next();
+                if let Some(_) = iter.next() {
+                    ResultValue::Error(String::from("Panic to many parameters for define"))
+                } else {
+                    ResultValue::Int(10)
+                }
+            }
+            _ => {
+                ResultValue::Error(String::from("unknown atom"))
+            }
+        }
+    }
 }
--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -1,87 +1,13 @@
 pub mod environment;
 
-use std::slice::Iter;
-
-use parser::{Parser, Token};
+use parser::Parser;
 
 use self::environment::{Environment, ResultValue};
 
-
-pub fn eval(code: &str) -> ResultValue {
+pub fn eval<'a>(code: &str) -> ResultValue {
     let parser = Parser::parse(code);
-    let environment = Environment::new();
-    eval_parser(&parser.tree, &environment)
-}
-
-fn eval_parser(tree: &Vec<Token>, environment: &Environment) -> ResultValue {
-    let mut iter = tree.into_iter();
-    let mut result = ResultValue::None;
-    while let Some(token) = iter.next() {
-        match token {
-            Token::Atom(symbol) => {
-                result = match_atom(symbol, &mut iter, environment);
-            },
-            Token::Subs(subs) => {
-                result = eval_parser(subs, &environment.child());
-            }
-            _ => {
-                println!("Panic at {:?}", tree);
-                result = ResultValue::None;
-            }
-        }
-    }
-
-    result
-}
-
-fn match_atom(symbol: &String, iter: &mut Iter<Token>, environment: &Environment) -> ResultValue {
-    let str_symbol = symbol.as_str();
-    match str_symbol {
-        "cons" => {
-            let mut result = String::new();
-            while let Some(token) = iter.next() {
-                match token {
-                    Token::AtomString(atom) => result.push_str(atom.as_str()),
-                    _ => result.push_str("Wrong parameters for 'cons'")
-                };
-            };
-            ResultValue::Str(result)
-        },
-        "+" => {
-            let mut result = 0.0;
-            let mut int_only = true;
-            while let Some(token) = iter.next() {
-                match token {
-                    Token::AtomInt(atom) => result += *atom as f64,
-                    Token::AtomFloat(atom) => {
-                                int_only = false;
-                                result += atom
-                    },
-                    Token::Subs(tokens) => {
-                        let result_value = eval_parser(tokens, &environment.child());
-                        match result_value {
-                            ResultValue::Int(atom) => result += atom as f64,
-                            ResultValue::Float(atom) => {
-                                int_only = false;
-                                result += atom
-                            },
-                            _ => println!("Panic")
-                        }
-                    },
-                    _ => println!("Panic")
-                };
-            };
-            if int_only {
-                ResultValue::Int(result as i64)
-            } else {
-                ResultValue::Float(result)
-            }
-        },
-        _ => {
-            println!("unknown atom");
-            ResultValue::Error()
-        }
-    }
+    let mut environment = Environment::new();
+    environment.eval_parser(&parser.tree)
 }
 
 
@@ -93,8 +19,8 @@ mod test {
   #[test]
     fn test_define_with_add() {
         let runtime = eval(r#"
-        (+ 10 10)
-        (+ 30 40)"#);
+        (define r 30)
+        (+ r 40)"#);
         match runtime {
             ResultValue::None => assert!(false),
             ResultValue::Int(result) => assert_eq!(result, 70),
@@ -105,12 +31,40 @@ mod test {
         }
     }
 
+  #[test]
+    fn test_define() {
+        let runtime = eval(r#"(define r 30)"#);
+        match runtime {
+            ResultValue::None => assert!(false),
+            ResultValue::Error(_) => assert!(true),
+            _ => {
+                println!("{:?} Error", runtime);
+                assert!(false, "No error");
+            }
+        }
+    }
+
+
+
+  #[test]
+    fn test_define_too_many_parameters() {
+        let runtime = eval(r#"(define r 30 40)"#);
+        match runtime {
+            ResultValue::None => assert!(false),
+            ResultValue::Error(_result) => assert!(true),
+            _ => {
+                println!("{:?} Error", runtime);
+                assert!(false, "No error");
+            }
+        }
+    }
+
     #[test]
     fn test_cons() {
         let runtime = eval(r#"(cons "Hello " "du " "da")"#);
         match runtime {
             ResultValue::None => assert!(false),
-            ResultValue::Str(result) => assert_eq!(result, String::from("Hello du da")),
+            ResultValue::Str(result) => assert_eq!(*result, String::from("Hello du da")),
             _ => assert!(false, "It's not a string")
         }
     }
