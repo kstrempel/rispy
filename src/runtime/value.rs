039bc7d29c947694236cfd1ef681d4edf61f73af
Kai Strempel <kstrempel@gmail.com>
2018-11-11 16:45:04 +0100

    only one enum instead of one for parsen and vm

--- src/runtime/value.rs
+++ src/runtime/value.rs
@@ -1,12 +1,38 @@
+use std::vec::Vec;
+
 
 #[derive(Debug)]
-pub enum Value {
+pub enum Token {
     None,
     Int(i64),
     Float(f64),
     Str(String),
     Atom(String),
     Func(),
+    Block(Vec<Token>),
     Error(String),
 }
 
+impl Token {
+    pub fn atom_2_string(&self) -> Result<String, Token> {
+        match self {
+            Token::Atom(s) => Ok(s.clone()),
+            _ => Err(Token::Error(String::from("Must be a string")))
+        }
+    }
+}
+
+impl Clone for Token {
+    fn clone(&self) -> Self {
+        match self {
+            Token::None => Token::None,
+            Token::Float(f) => Token::Float(*f),
+            Token::Int(i) => Token::Int(*i),
+            Token::Str(s) => Token::Str(s.clone()),
+            Token::Atom(s) => Token::Atom(s.clone()),
+            Token::Func() => Token::Func(),
+            Token::Block(vec) => Token::Block(vec.clone()),
+            Token::Error(s) => Token::Error(s.clone())
+        }
+    }
+}
